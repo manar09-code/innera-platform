365eb0be5ae497b8feb7c23531adf3865b4b1472
Manar Degachi <manardg09@gmail.com>
2026-01-08 16:09:15 +0100

    Generate 8 Angular components (login, register, home, feed, forgot-password, reset-password)

---
+++ src/app/pages/register-admin/register-admin.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-admin',
+  imports: [],
+  templateUrl: './register-admin.html',
+  styleUrl: './register-admin.css',
+})
+export class RegisterAdmin {
+
+}
